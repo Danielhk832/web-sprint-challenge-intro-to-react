732bbd788147af21412a31f27029d40d8f727151
Daniel Kish <aox.soljah@gmail.com>
2021-09-11 02:37:35 +0700

    pulled character names and rendered to DOM. codegrade MVP met, styling still need to be completed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import axios from 'axios';
+import gsap from 'gsap';
+import Character from './components/Character'
+
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -9,10 +13,25 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  const [characters, getCharacters] = useState([])
+  const [films, getFilms] = useState([])
+  
+  useEffect(() => {
+    axios.get('https://swapi.dev/api/people') 
+    .then(res => {
+      getCharacters(res.data)
+      console.log(characters)
+    })
+}, [])
+
   return (
-    <div className="App">
-      <h1 className="Header">Characters</h1>
-    </div>
+      <div className="App"> 
+        <h1 className="Header">Characters</h1>
+      { characters.map(ch => {
+        return <Character name={ch.name} bday={ch.birth_year} gender={ch.gender} />
+      })
+      }
+      </div>
   );
 }
 
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,26 @@
 // Write your Character component here
+import styled from 'styled-components';
+import gsap from 'gsap';
+
+const Styling = styled.div`
+    color: #443e3e;
+    background-color: rgba(255, 255, 255, .5);
+    text-shadow: 1px 1px 5px yellow;
+
+    .container {
+        width: 50%;
+    }
+`
+
+export default function Details(props) {
+    const {name, bday, gender} = props;
+    return(
+        <Styling>
+            <div className="container">
+                <h2>{name}</h2>
+                <p>Birth year: {bday}</p>
+                <p>Gender: {gender}</p>
+            </div>
+        </Styling>
+    )
+}
